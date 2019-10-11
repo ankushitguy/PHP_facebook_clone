b5d3c81e8b937b39840cd35899bf2156e21c6e23
ankushitguy <33928857+ankushitguy@users.noreply.github.com>
2019-10-11 21:49:42 +0530

    test website

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Facebook</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <div class="max--container">
+            <nav>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quibusdam, reprehenderit. Atque autem consequuntur dolor id ipsam maxime nostrum soluta velit vitae voluptate. Earum non nulla perferendis quidem repudiandae voluptate.
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="max--container">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci debitis, eaque excepturi explicabo illo ipsam necessitatibus neque quam quisquam sequi sit sunt vero vitae. Aperiam architecto fugit qui?
+        </div>
+    </main>
+    <footer>
+        <div class="max--container">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus beatae eius enim esse fuga, ipsam perspiciatis rerum? Ab, aliquam eligendi explicabo incidunt ipsam magni, natus nobis officia quos vero voluptatem.
+        </div>
+    </footer>
+</body>
+</html>
